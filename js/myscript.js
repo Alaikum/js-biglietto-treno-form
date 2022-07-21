a80719e6c8a8507c934b687cf9a8b75d9ad6cc05
Alaikum <carangiadamo@gmail.com>
2022-07-21 15:25:06 +0200

    Input km, input età e button done

--- js/myscript.js
+++ js/myscript.js
@@ -9,3 +9,29 @@
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
+
+const prezzoAlKm = 0.21
+
+
+
+const buttonElement = document.querySelector('.calcola');
+
+buttonElement.addEventListener('click', function () {
+    // alert("non toccare")
+    const distanzaInput = document.querySelector(".distanza")
+    const etaInput = document.querySelector('.eta');
+    const eta= etaInput.value
+    
+
+
+
+
+
+    const prezzo = distanzaInput.value * prezzoAlKm
+    const text = document.getElementById('we');
+    text.innerHTML = prezzo + 'Euro'
+
+
+})
+
+
